aebaebf10b8762e7bd44dd682baaa05e77181282
Corinne Mestemacher <corimest@Corinnes-MacBook-Air.local>
2022-07-30 16:35:50 -0500

    post routes

--- models/index.js
+++ models/index.js
@@ -1,4 +1,15 @@
 const User = require('./User');
 const Post = require("./Post"); 
 
+// create associations, user can have many posts
+User.hasMany(Post, {
+    // creates reference for  id column in USer model to link to corresponding foreign key pair (user_id in Post model)
+    foreignKey: 'user_id'
+});
+
+//reverse association, Post can only belong to 1 user (1 to 1)
+Post.belongsTo(User, {
+    foreignKey: 'user_id', 
+});
+
 module.exports = { User, Post };
--- server.js
+++ server.js
@@ -12,6 +12,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(routes);
 
 // turn on connection to db and server
-sequelize.sync({ force: true }).then(() => {
+// force: true makes tables re-create if there are any association changes
+sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
 });
